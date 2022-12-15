e3cdc80c82bef83a0134d67c08868d29b835388b
oux17 <31462144@qq.com>
2022-12-15 16:18:00 +0800

    新增 list

--- src/main/java/com/util/ResultDTO.java
+++ src/main/java/com/util/ResultDTO.java
@@ -3,10 +3,15 @@ package com.util;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
+
 @Data
 public class ResultDTO implements Serializable {
 
     private int code = 0;
 
     private String msg;
+
+    List<Object> list;
+
 }
